2ef3d7673445659db9b24d8c166463a539267c6e
Sajid Ibne Anower <sajidanower23@gmail.com>
2018-05-22 14:34:37 +1000

    Add newline

--- app/Main.hs
+++ app/Main.hs
@@ -1,10 +1,10 @@
- {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE OverloadedStrings #-}
 
-import Web.Scotty
+import           Web.Scotty
 
-import Data.Monoid (mconcat)
+import           Data.Monoid (mconcat)
 
 main = scotty 3000 $ do
     get "/:word" $ do
         beam <- param "word"
-        html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+        html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>\n"]
